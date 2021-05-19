cd87036bf52b1a4b75b0ae803161949e00fe5e9f
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2021-05-18 21:47:08 -0300

    Adds dependencies markdown, django-filter and pygments

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # drf-feautures-testintg
 
 Repository to test the Django Rest Framework features
+
+# Quickstart
+[Quickstart](https://www.django-rest-framework.org/tutorial/quickstart/)
+
+Install dependencies
+```bash
+$ pipenv install django djangorestframework markdown django-filter pygments
+$ pipenv shell
+````
+
+Start the project
+```bash
+$ django-admin startproject tutorial .
+$ cd tutorial
+$ django-admin startapp quickstart
+$ cd ..
+$ python manage.py migrate
+$ python manage.py createsuperuser --email admin@example.com --username admin
+$ python manage.py runserver
+```
--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,16 +1,26 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
+    # UserViewSet
     API endpoint that allows users to be viewed or edited.
+
+    ```python
+    permission_classes = [IsAuthenticated | ReadOnly]
+    ```
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated | ReadOnly]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
@@ -19,4 +29,4 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
