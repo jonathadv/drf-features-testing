bbef20938b5fdfdbd55b2a649964a7971cc107cd
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2021-05-19 23:20:19 -0300

    Adds pylint

--- setup.py
+++ setup.py
@@ -5,6 +5,31 @@ import os
 import sys
 from setuptools import setup, Command
 
+MODULE_NAMES = "tutorial snippets"
+
+
+class LintCommand(Command):
+    """Run pylint"""
+
+    description = "Run pylint."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.status("Running Pylint...")
+        os.system(f"pipenv run pylint {MODULE_NAMES}".format(sys.executable))
+        sys.exit()
+
 
 class FormatCommand(Command):
     """Format code with Black"""
@@ -25,7 +50,7 @@ class FormatCommand(Command):
 
     def run(self):
         self.status("Formatting...")
-        os.system("black .".format(sys.executable))
+        os.system("pipenv run black .".format(sys.executable))
         sys.exit()
 
 
@@ -33,5 +58,6 @@ class FormatCommand(Command):
 setup(
     cmdclass={
         "format": FormatCommand,
+        "lint": LintCommand,
     },
 )
